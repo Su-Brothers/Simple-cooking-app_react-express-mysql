fed6564af4d3d3600a43a276bd98d1b9c69e26f8
dbehdgjs123 <dbehdgjs12345@gmail.com>
2020-08-12 00:01:55 +0900

    디렉터리 틀잡기 및 react route 구현 (종속성 설치요함)

---
+++ client/src/components/MainPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./styles/mainpage.scss";
+function MainPage() {
+  return <div className="main-container">메인화면</div>;
+}
+
+export default MainPage;
---
+++ client/src/components/NavBar.js
@@ -0,0 +1,52 @@
+import React from "react";
+import "./styles/nav-var.scss";
+import {
+  FaCarrot,
+  FaSearch,
+  FaUserAlt,
+  FaUtensils,
+  FaCog,
+} from "react-icons/fa";
+function NavBar() {
+  return (
+    <header>
+      <div className="header-left-menu">
+        <div className="header-title-container">
+          <FaCarrot className="header-title-icon" /> {/*임시*/}
+          <span
+            style={{
+              color: "#f57c00",
+              fontSize: "1.7rem",
+              transform: "rotate(10deg)",
+            }}
+          >
+            자
+          </span>
+          취요리
+          <span
+            style={{
+              color: "#388e3c",
+              fontSize: "1.7rem",
+              transform: "rotate(-10deg)",
+            }}
+          >
+            백
+          </span>
+          과사전
+        </div>
+        <div className="header-search">
+          <FaSearch className="header-search-icon" />
+          <input type="text" placeholder="레시피를 입력하세요..." />
+        </div>
+      </div>
+
+      <div className="header-right-menu">
+        <FaUserAlt />
+        <FaUtensils />
+        <FaCog />
+      </div>
+    </header>
+  );
+}
+
+export default NavBar;
