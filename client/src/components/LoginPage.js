3d7f562e8e11c8d9a127753b42e431e4a8f0e7b8
dbehdgjs123 <dbehdgjs12345@gmail.com>
2020-08-19 12:06:11 +0900

    로그인 기본 디자인 완성

--- client/src/components/LoginPage.js
+++ client/src/components/LoginPage.js
@@ -5,14 +5,22 @@ function LoginPage() {
   return (
     <>
       <div className="login_container">
-        <div className="login_container_img">
+        <div className="login_container_logo">
+          <div className="login_container_logo_title">환영합니다.</div>
           <img src={logo} alt="logo" />
         </div>
         <div className="login_container_info">
-          LOGIN
+          <div className="login_container_info_title">LOGIN</div>
           <form>
             <input type="text" name="userId" placeholder="아이디" />
             <input type="text" name="userPassword" placeholder="비밀번호" />
+            <button type="submit" className="login_btn">
+              로그인
+            </button>
+            <div className="hr-text">계정이 없으신가요?</div>
+            <button type="submit" className="singup_btn">
+              회원가입
+            </button>
           </form>
         </div>
       </div>
