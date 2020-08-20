6a58aab96bac5fde1dfa1175117eaedf80aad376
Jungsoo Kim <sunpl13@naver.com>
2020-08-20 22:29:54 +0900

    Make Minitimeline Component & Apply RankPage

--- client/src/components/MainPage.js
+++ client/src/components/MainPage.js
@@ -3,10 +3,7 @@ import "./styles/mainpage.scss";
 import MiniTimeline from "./MiniTimeline"
 function MainPage() {
   return (
-    <div>
   <div className="main-container">메인화면</div>
-  <MiniTimeline/>
-  </div>
   )
 }
 
--- client/src/components/RankPage.js
+++ client/src/components/RankPage.js
@@ -3,7 +3,8 @@ import Cooking from '../icons/cooking.svg';
 import "./styles/rankpage.scss";
 import {FaAngleLeft, FaAngleRight} from 'react-icons/fa';
 import MiniTimeline from './MiniTimeline';
-import TimeLine from './TimeLine';
+import TimeLine from './TimeLine'
+
 
 const TOTAL_SLIDES = 3;
 
@@ -60,27 +61,35 @@ function RankPage() {
     }
  
     return (
-    <div >
-        <div className="pictures">
-            <img src = {Cooking} width = "100px" heigh = "100px"/>
-        </div>
-        <div className="controls_box">
-            <div className = "controls">
-              <a className = "prev"   onClick = {prevBtn}><FaAngleLeft/></a>
-              <a className = "next" onClick = {nextBtn}><FaAngleRight/></a>
+    <div className = "rank_container">
+        <div className = "rank_container_menu">
+            <div className="pictures">
+             <img src = {Cooking} width = "100px" heigh = "100px"/>
             </div>
+            <div className="controls_box">
+             <div className = "controls">
+                  <a className = "prev"   onClick = {prevBtn}><FaAngleLeft/></a>
+                <a className = "next" onClick = {nextBtn}><FaAngleRight/></a>
+                </div>
             
+            </div>
+         <div className ="slide_wrapper">
+               <ul className = "slides" style = {{transition: `${transIdx}s ease-out`, transform:`translateX(${(currentIdx * -150) -150}px)`}}>
+                    <li>양식</li>
+                    <li>한식</li>
+                    <li>중식</li>
+                   <li>일식</li>
+                   <li>양식</li>
+             </ul>
+            </div>
         </div>
-        <div className ="slide_wrapper">
-            <ul className = "slides" style = {{transition: `${transIdx}s ease-out`, transform:`translateX(${(currentIdx * -150) -150}px)`}}>
-                <li>양식</li>
-                <li>한식</li>
-                <li>중식</li>
-                <li>일식</li>
-                <li>양식</li>
-            </ul>
-        </div>
-            <div className = "content_container"> <TimeLine/></div>
+            <div className = "minitimeline_list">
+            <div  className = "content_container"><MiniTimeline/></div>
+            <div  className = "content_container"><MiniTimeline/></div>
+            <div  className = "content_container"><MiniTimeline/></div>
+            <div  className = "content_container"><MiniTimeline/></div>
+            <div  className = "content_container"><MiniTimeline/></div>
+            </div>
     </div>
     )
 }
